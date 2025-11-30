47e9dd86dc59e90ba4d5465c1cea85778bb848cf
PreethamMR-code <preethampreetham560@gmail.com>
2025-11-30 23:58:49 +0530

    JDBC

---
+++ src/main/java/com/xworkz/data/constants/DBConstant.java
@@ -0,0 +1,7 @@
+package com.xworkz.data.constants;
+
+public class DBConstant {
+    static final String URL = "jdbc:mysql://localhost:3306/matrimony_db";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "0000";
+}
