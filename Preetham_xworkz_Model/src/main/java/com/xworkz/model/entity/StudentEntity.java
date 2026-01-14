084772381fd959656f45f39c44a09f79fd658a62
PreethamMR-code <preethampreetham560@gmail.com>
2026-01-15 02:27:56 +0530

    Entity and table

---
+++ Preetham_xworkz_Model/src/main/java/com/xworkz/model/entity/StudentEntity.java
@@ -0,0 +1,29 @@
+package com.xworkz.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "students")
+public class StudentEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+
+        private String name;
+        private String email;
+        private String phone;
+        private int age;
+        private String gender;
+        private String address;
+        private String password;
+
+
+}
