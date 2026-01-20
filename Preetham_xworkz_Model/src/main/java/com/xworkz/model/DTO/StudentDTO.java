e7f8b410bc8a232c352e129ae29d9091d1f214dd
PreethamMR-code <preethampreetham560@gmail.com>
2026-01-20 19:49:31 +0530

    implemented DTO validation

--- Preetham_xworkz_Model/src/main/java/com/xworkz/model/DTO/StudentDTO.java
+++ Preetham_xworkz_Model/src/main/java/com/xworkz/model/DTO/StudentDTO.java
@@ -11,40 +11,43 @@ import javax.validation.constraints.*;
 @NoArgsConstructor
 public class StudentDTO {
 
-        @NotNull(message = "Name is required")
+        @NotNull(message = "")
         @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters")
         private String name;
 
-        @NotNull(message = "Email is required")
+        @NotNull(message = "")
         @Pattern(
                 regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
                 message = "Enter a valid email address"
         )
         private String email;
 
-        @NotNull(message = "Phone number is required")
-        @Digits(integer = 10, fraction = 0, message = "Phone number must be 10 digits")
+        @NotNull(message = "")
+        @Pattern(
+                regexp = "^[6-9][0-9]{9}$",
+                message = "Phone must start with 6–9 and be 10 digits"
+        )
         private String phone;
 
-        @NotNull(message = "Age is required")
+        @NotNull(message = "")
         @Min(value = 18, message = "Age must be at least 18")
         @Max(value = 60, message = "Age must not exceed 60")
         private Integer age;   // ✔ Wrapper type (important)
 
-        @NotNull(message = "Gender is required")
+        @NotNull(message = "")
         @Pattern(regexp = "[(Male|Female|Other)]", message = "Gender must be M or F")
         private String gender;
 
-        @NotNull(message = "Address is required")
+        @NotNull(message = "")
         @Size(min = 5, max = 100, message = "Address must be between 5 and 100 characters")
         private String address;
 
-        @NotNull(message = "Password is required")
+        @NotNull(message = "")
         @Size(min = 6, max = 15, message = "Password must be 6–15 characters")
         private String password;
 
-        // ❌ Not stored in DB
-        @NotNull(message = "Confirm Password is required")
+        //  Not stored in DB
+        @NotNull(message = "")
         private String confirmPassword;
 
 
