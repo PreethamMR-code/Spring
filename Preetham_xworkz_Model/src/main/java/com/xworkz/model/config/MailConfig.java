ad0bd90482dddc6a7136831f8c41d15750092bb9
PreethamMR-code <preethampreetham560@gmail.com>
2026-01-22 20:23:37 +0530

    configuaration for mail through google password key

---
+++ Preetham_xworkz_Model/src/main/java/com/xworkz/model/config/MailConfig.java
@@ -0,0 +1,31 @@
+package com.xworkz.model.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender(){
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("preethampreetham560@gmail.com");
+        mailSender.setPassword("tcem xiqh omay xxjp");
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "false");
+
+        return mailSender;
+    }
+}
