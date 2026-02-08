eecb3dd3318ac9530d6b3ae498087d5bf3b7e73f
PreethamMR-code <preethampreetham560@gmail.com>
2026-02-08 23:26:35 +0530

    new entity implemented

---
+++ Preetham_xworkz_Model/src/main/java/com/xworkz/model/entity/FileEntity.java
@@ -0,0 +1,25 @@
+package com.xworkz.model.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "uploaded_files")
+public class FileEntity {
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+
+        private String originalFileName;
+        private String storedFilePath; // Full path on D: drive
+        private long fileSize;
+        private String contentType;
+        private LocalDateTime uploadedAt = LocalDateTime.now();
+    }
+}
