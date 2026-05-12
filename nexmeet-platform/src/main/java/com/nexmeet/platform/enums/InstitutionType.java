c92969cb76940a80273deac432d31404fcc64de5
PreethamMR-code <preethampreetham560@gmail.com>
2026-05-12 23:04:43 +0530

    feat(phase37): Institution Management
    InstitutionType enum: COLLEGE, UNIVERSITY, COMPANY, NGO, GOVERNMENT, OTHER

--- nexmeet-platform/src/main/java/com/nexmeet/platform/enums/InstitutionType.java
+++ nexmeet-platform/src/main/java/com/nexmeet/platform/enums/InstitutionType.java
@@ -1,5 +1,17 @@
 package com.nexmeet.platform.enums;
 
+
+/*
+ * Type of institution.
+ * COLLEGE     = Engineering/Arts/Science colleges
+ * UNIVERSITY  = Universities
+ * COMPANY     = Private companies (for employee bulk-reg)
+ * NGO         = Non-profits
+ * GOVERNMENT  = Govt offices, PSUs
+ * OTHER       = Anything else
+ */
+
+
 public enum InstitutionType {
     COLLEGE,
     UNIVERSITY,
